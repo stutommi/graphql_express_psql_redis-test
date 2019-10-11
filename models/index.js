8b5e5034452afd462c9ea88deb6813302f354e09
stutommi <tommi.teetee@hotmail.com>
2019-10-11 13:24:54 +0300

    fix syntax error

--- models/index.js
+++ models/index.js
@@ -23,7 +23,7 @@ const syncDb = async (sequelize, tries = 5) => {
       console.log('could not connect to database')
       process.exit(1)
     }
-    console.log('err:', e.message, ',trying again in 3 seconds,' tries, 'tries left')
+    console.log('err:', e.message, ',trying again in 3 seconds,', tries, 'tries left')
     await wait(3000)
     syncDb(tries - 1)
   }
